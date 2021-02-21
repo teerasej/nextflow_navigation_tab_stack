e0efc89559c0d368c9e08a56066a0b8cf6a1b428
Teerasej <teerasej@gmail.com>
2021-02-21 21:04:42 +0700

    create contact deatil page with parameter passing

---
+++ lib/pages/contact_detail_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nextflow_navigation_tab_stack/models/contact_model.dart';
+
+class ContactDetailPage extends StatelessWidget {
+  final ContactModel contact;
+
+  ContactDetailPage(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
